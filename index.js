deed853788075c852b2e3856f9c2a17928dd8abe
shashank <shashank.srivastava5523@gmail.com>
2022-10-25 14:07:01 +0530

    3rd commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log("Backend server is running!");
   });
   
